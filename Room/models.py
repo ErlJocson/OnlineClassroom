c4ab8c6823d28a1ae5e8c6cbc5f8f686aecb122b
ErlJocson <jocsonerl@gmail.com>
2022-05-07 23:00:18 +0800

    added new designs

--- Room/models.py
+++ Room/models.py
@@ -1,6 +1,15 @@
 from django.db import models
 from UserAuth.models import UserClassification
-from .functions import generateRandomId
+import random
+import string
+
+def generateRandomId():
+    while True:
+        characters = string.ascii_letters + string.digits + string.punctuation
+        password = ''.join(random.choice(characters) for i in range(8))
+        roomId = Room.objects.get(roomId=password).exists()
+        if not roomId:
+            return password
 
 class Room(models.Model):
     name = models.CharField(max_length=24)
