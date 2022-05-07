f752ced0267d6ad1c06c2a124a3c165a9df6cfc1
ErlJocson <jocsonerl@gmail.com>
2022-05-07 22:55:40 +0800

    seperated the random id generator

--- Room/models.py
+++ Room/models.py
@@ -1,17 +1,12 @@
 from django.db import models
 from UserAuth.models import UserClassification
-import random
-import string
-
-def generateRandomId():
-    while True:
-        characters = string.ascii_letters + string.digits + string.punctuation
-        password = ''.join(random.choice(characters) for i in range(8))
-        roomId = Room.objects.get(roomId=password).exists()
-        if not roomId:
-            return password
+from .functions import generateRandomId
 
 class Room(models.Model):
     name = models.CharField(max_length=24)
     roomId = models.CharField(max_length=10, default=generateRandomId)
     teacher = models.ForeignKey(UserClassification, on_delete=models.CASCADE)
+
+class ListOfStudent(models.Model):
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    student = models.ForeignKey(UserClassification, on_delete=models.CASCADE)
